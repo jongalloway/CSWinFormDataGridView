4c4f38e7cab183310767a6e46799d9e66ea3b1a4
Jon Galloway <jongalloway@gmail.com>
2025-09-07 17:03:11 -0700

    Reset to original .NET Framework 3.5 and removed other forms

--- Program.cs
+++ Program.cs
@@ -14,11 +14,10 @@ namespace CSWinFormDataGridView
         static void Main()
         {
             Application.EnableVisualStyles();
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.SetCompatibleTextRenderingDefault(false);
 
             //Application.Run(new CustomDataGridViewColumn.MainForm());
-            Application.Run(new DataGridViewPaging.MainForm());
+            //Application.Run(new DataGridViewPaging.MainForm());
             //Application.Run(new EditingControlHosting.MainForm());
             //Application.Run(new JustInTimeDataLoading.MainForm());
             //Application.Run(new MultipleLayeredColumnHeader.MainForm());
